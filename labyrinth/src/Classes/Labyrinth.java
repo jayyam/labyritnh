fe0d04ecdd486fea5acfb24638cba935c64bddb6
meursaulDAW <omardpana22@gmail.com>
2022-02-28 19:29:48 +0100

    RetoquesDavid

--- labyrinth/src/Classes/Labyrinth.java
+++ labyrinth/src/Classes/Labyrinth.java
@@ -19,14 +19,14 @@ public class Labyrinth {
     private boolean loaded;
     private int startI, startJ, endI, endJ;
     private ArrayList<Coordinate> path = new ArrayList<Coordinate>();
+
     /**
      * PATH
      * utilizará el algoritmo para guardar las coordenadas y dirección del camino a buscar
      * para luego mostrarlo
-     * */
+     */
 
-    public Labyrinth()
-    {
+    public Labyrinth() {
         loaded = false;
     }
 
@@ -77,6 +77,7 @@ public class Labyrinth {
         }
         return lines;
     }
+
     private File[] listDirectory() throws IOException {
         File[] files = new File[0];
         try {
@@ -87,59 +88,28 @@ public class Labyrinth {
         return files;
     }
 
-    public void showMap() throws IOException, InterruptedException
-    {
-        if (!loaded)
-        {
+    public void showMap() throws IOException, InterruptedException {
+        if (!loaded) {
             System.err.print("No se ha cargado el laberinto");
-        }
-        else if(startI == startJ && endI == endJ)//No se han establecido las casillas de entrada y salida
-        {
-            System.err.print("Debe establecer las casillas de entrada y salida");
-        }
-        else
-        {
-            for (int i = 0; i < map.length; i++)
-            {
-                    for (int j = 0; j < map[i].length; j++)
-                    {
-                        ArrayList<Coordinate> path = new ArrayList<Coordinate>();
+        } else {
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++)
+                {
+                    if (startI == i && startJ == j) {
+                        System.out.print("E ");
+                        continue;
+                    } else if (endI == i && endJ == j) {
+                        System.out.print("S ");
+                        continue;
+                    } else {
+                        for(int k = 0; k < path.size(); k++){
+                            if() { //Comprobar si en la posicion "k", la i coincide con la i y la j con la j
 
-                        if (startI == i && startJ == j)
-                        {
-                            System.out.print("E ");
-                        }
-                        else if (endI == i && endJ == j)
-                        {
-                            System.out.print("S ");
-                        }
-                       else
-                        {
-
-
-                            /**
-                             * ArrayList<String> users = readUserFile();
-                             * 		//System.out.println("A");//test
-                             * 		for (int i = 0; i < users.size(); i++) {
-                             * 			String[] currentUser = users.get(i).split("#");
-                             * 			//System.out.println("B " + users.get(i));//test
-                             * 			if (username.equalsIgnoreCase(currentUser[0]) && password.equals(currentUser[1])) {
-                             * 				//System.out.println("C");//test
-                             * 				logged = true;
-                             * 				setUser(currentUser);
-                             * 				System.out.println(username + "\nHa iniciado sesion con exito!");
-                             * 				break;
-                             *                        }* 		}
-                             * 		//System.out.println("D");//test
-                             * 		if (!logged) {
-                             * 			//System.out.println("Z");//test
-                             * 			System.err.println(username + "\nUsuario o Contraseña incorrecto!");
-                             * 		}
-                             */
-
-                            System.out.print(map[i][j] + " ");
+                            }
                         }
                     }
+                    System.out.print(map[i][j] + " ");
+                }
                 System.out.println();
             }
         }
@@ -168,159 +138,119 @@ public class Labyrinth {
             endI = iS;
             endJ = jS;
         }
-        /**
-         si loaded == false --> ERROR --> FIN
-
-         -pedir i y j de entrada (crear las variables, todavia no meterlas en las generales)
-         si (i o j son < 0)                            |
-         si (i o j son >= longitud de las dimensiones) | ERROR --> FIN
-         si (map[sI][sJ] == '#')                       |
-
-         Pedir i y j de salida
-         a- "las mismas de arriba "			|
-         b- "las mismas de arriba "			| ERROR --> FIN
-         c- "las mismas de arriba"			|
-         d- Si eI == sI && eJ == sJ   		|
-
-         -Meter valores en startI,startJ,endI,endJ
-         */
     }
-    public  void selectAlgorithm()//Metodo david Lunes28F
-    {
-        /**
-         if (!loaded) //Comprobaciones iniciales ya hechas en metodo selectEntranceExit
-        {
-            System.err.print("No se ha cargado el laberinto");
-        }
 
-        if (startI == startJ && endI == endJ)//movido a selectEntranceExit en el else if
-        {
-            System.err.print("Debe establecer las casillas de entrada y salida");
-        }
-         */
-
-    }
     public void coordinateMenu() throws IOException, InterruptedException {
         System.out.println(Config.MENUFINDPATH);
-        for (int i = 0; i < 3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             int option = Interface.getInt("Selecciona opcion: ");
-            if (option < 0 || option >= 3)
-            {
+            if (option < 0 || option >= 3) {
                 System.err.println("Opcion introducida no valida");
                 continue;
             }
-            if (option == 0)
-            {
+            if (option == 0) {
                 return;
-            }
-            else
-            {
-                if (option == 1)
-                {
+            } else {
+                if (option == 1) {
                     //Metodo Primer camino encontrado
-                    if (findFirstPath())
+                    if (startI == startJ && endI == endJ)//No se han establecido las casillas de entrada y salida
                     {
+                        System.err.print("Debe establecer las casillas de entrada y salida");
+                    } else if (findFirstPath()) {
                         System.out.println("Primer camino encontrado");
                         showMap();
-                    }
-                    else
-                    {
+                    } else {
                         System.err.println("No se ha encontrado camino");
                     }
-                }
-                else
-                {
+                } else {
                     //Metodo Camino mas rapido
                 }
             }
         }
     }
 
-    private Coordinate setNextCell(Coordinate currentCoordinate)
-    {
-        Coordinate nextCell= new Coordinate();
-        nextCell.i = currentCoordinate.i;
-        nextCell.j = currentCoordinate.j;
-        nextCell.direction = 0;
-
-        if(currentCoordinate.direction == 1)
-        {
-                nextCell.i--;
-                //^ = 1 = Arriba = i-1
-        }
-        else if(currentCoordinate.direction == 2)
-        {
-                nextCell.i++;
-                //v = 2 = Abajo = i+1
-        }
-        else if(currentCoordinate.direction == 3)
-        {
-                nextCell.j--;
-                //< = 3 = Izquierda = j-1
-        }
-        else if(currentCoordinate.direction == 4)
-        {
-                nextCell.j++;
-                //> = 4 = Derecha = j+1
-        }
-        return nextCell;
-    }
-
-    private boolean findFirstPath()
-    {
+    private boolean findFirstPath() {
         boolean found = false;
 
         path = new ArrayList<Coordinate>();//limpiando el path
 
-        Coordinate startCell= new Coordinate();//creando objeto tipo coordenada
-        startCell.i = startI;//entrando en path la casilla de inicio
-        startCell.j = startJ;
-        startCell.direction = 0;
+        Coordinate startCell = new Coordinate();//creando objeto tipo coordenada
+        startCell.i = startI;//entrando en el path la casilla de inicio (i)
+        startCell.j = startJ;//entrando en el path la casilla de inicio (i)
+        startCell.direction = 0;//Estableciendo la direccion de inicio en 0
 
-        while(!found && path.size() > 0)
-        {
-            path.get(path.size()-1).direction += 1;//posicion inicial del laberinto incrementado en 1 posicion
+        while (!found && path.size() > 0) {
+            path.get(path.size() - 1).direction += 1;//posicion inicial del laberinto incrementado en 1 posicion
 
-            if(path.get(path.size()-1).direction <= 4);//comprobando entre 0 y 4 la direccion a moverse tras incrementar 1 posicion
+            if (path.get(path.size() - 1).direction <= 4)
+                ;//comprobando entre 0 y 4 la direccion a moverse tras incrementar 1 posicion
             {
-                Coordinate nextCell = setNextCell(path.get(path.size()-1));
-                if(checkCell(nextCell))
-                {
+                Coordinate nextCell = setNextCell(path.get(path.size() - 1));
+                if (checkCell(nextCell)) {
                     path.add(nextCell);
-                    if (nextCell.i == endI && nextCell.j == endJ)
-                    {
+                    if (nextCell.i == endI && nextCell.j == endJ) {
                         found = true;
                     }
-                }
-                else
-                {
-                    path.remove(path.size()-1);//borrando la ultima posicion
+                } else {
+                    path.remove(path.size() - 1);//borrando la ultima posicion
                 }
             }
         }
         return found;
     }
 
-    private boolean checkCell(Coordinate cell)
-    {
+    private Coordinate setNextCell(Coordinate currentCoordinate) {
+        Coordinate nextCell = new Coordinate();
+        nextCell.i = currentCoordinate.i;
+        nextCell.j = currentCoordinate.j;
+        nextCell.direction = 0;
+
+        if (currentCoordinate.direction == 1) {
+            nextCell.i--;
+            //^ = 1 = Arriba = i-1
+        } else if (currentCoordinate.direction == 2) {
+            nextCell.i++;
+            //v = 2 = Abajo = i+1
+        } else if (currentCoordinate.direction == 3) {
+            nextCell.j--;
+            //< = 3 = Izquierda = j-1
+        } else if (currentCoordinate.direction == 4) {
+            nextCell.j++;
+            //> = 4 = Derecha = j+1
+        }
+        return nextCell;
+    }
+
+    private boolean checkCell(Coordinate cell) {
         if (map[cell.i][cell.j] == '#')//pared
-            {
-                return false;
-            }
-        for (int i=0;i<path.size();i++)
         {
-            if (path.get(i).i == cell.i && path.get(i).j == cell.j)//comprueba si ya ha estado en esta casilla
+            return false;
+        }
+        for (int i = 0; i < path.size(); i++) {
+            if (path.get(i).i == cell.i && path.get(i).j == cell.j) //comprueba si ya ha estado en esta casilla
             {
                 return false;
             }
         }
         return true;
     }
-
 }
 
-/**
+    /**private char getCell(int Ie, int Je)
+     * {
+        if
+        if
+        for
+            if
+            else if return '^'
+            else if return 'v'
+            else if return '<'
+            else if return '>'
+        }
+        return
+        }*/
+ //-----------------------------------------------
+ /**
  * Listado de acciones
  * -------------------
  * -Obtener listado ficheros
@@ -333,3 +263,56 @@ public class Labyrinth {
  * -Poner loaded a true y filename con su nombre
  * -Mensaje (CARGADO CORRECTAMENTE)
  */
+//--------------------------------------------------------------
+/**
+        * ArrayList<String> users = readUserFile();
+        * 		//System.out.println("A");//test
+        * 		for (int i = 0; i < users.size(); i++) {
+        * 			String[] currentUser = users.get(i).split("#");
+        * 			//System.out.println("B " + users.get(i));//test
+        * 			if (username.equalsIgnoreCase(currentUser[0]) && password.equals(currentUser[1])) {
+        * 				//System.out.println("C");//test
+        * 				logged = true;
+        * 				setUser(currentUser);
+        * 				System.out.println(username + "\nHa iniciado sesion con exito!");
+        * 				break;
+        *                        }* 		}
+        * 		//System.out.println("D");//test
+        * 		if (!logged) {
+        * 			//System.out.println("Z");//test
+        * 			System.err.println(username + "\nUsuario o Contraseña incorrecto!");
+        * 		}
+ */
+//---------------------------------------------------------------------
+/**
+ si loaded == false --> ERROR --> FIN
+
+ -pedir i y j de entrada (crear las variables, todavia no meterlas en las generales)
+ si (i o j son < 0)                            |
+ si (i o j son >= longitud de las dimensiones) | ERROR --> FIN
+ si (map[sI][sJ] == '#')                       |
+
+ Pedir i y j de salida
+ a- "las mismas de arriba "			|
+ b- "las mismas de arriba "			| ERROR --> FIN
+ c- "las mismas de arriba"			|
+ d- Si eI == sI && eJ == sJ   		|
+
+ -Meter valores en startI,startJ,endI,endJ
+ */
+//-----------------------------------------------------------------------------------
+/**public  void selectAlgorithm()//Metodo david Lunes28F
+ {
+
+ if (!loaded) //Comprobaciones iniciales ya hechas en metodo selectEntranceExit
+ {
+ System.err.print("No se ha cargado el laberinto");
+ }
+
+ if (startI == startJ && endI == endJ)//movido a selectEntranceExit en el else if
+ {
+ System.err.print("Debe establecer las casillas de entrada y salida");
+ }
+
+
+ }*/
