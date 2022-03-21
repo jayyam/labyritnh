0f38a521348b2e88114d4cab0015c49b364163f8
Daw20-09 <omardpana22@gmail.com>
2022-03-21 17:32:27 +0100

    1erCommit Github

---
+++ labyrinth/src/Classes/Database.java
@@ -0,0 +1,20 @@
+package Classes;
+
+public class Database
+{
+    public static login()
+    {
+        return User;
+    }
+
+    public static checkUser()
+    {
+        return User;
+    }
+
+    public static boolean signup()
+    {
+        if (){return true;}
+        else{return false;}
+    }
+}
---
+++ labyrinth/src/Classes/Utils.java
@@ -0,0 +1,37 @@
+package Classes;
+
+public class Utils
+{
+    public static boolean validateEmail()
+    {
+        return false;
+    }
+    public static boolean validatePassword()
+    {
+        return false;
+    }
+    public static boolean validateUsername()
+    {
+        return false;
+    }
+    public static boolean validateName()
+    {
+        return false;
+    }
+    public static boolean validateEmailDatabase()
+    {
+        return false;
+    }
+    public static boolean validateNif()
+    {
+        return false;
+    }
+    public static boolean validateDate()
+    {
+        return false;
+    }
+    public static void encryptMd5()
+    {
+
+    }
+}
