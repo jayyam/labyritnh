f6ea60a570e867b4b06f4f6aab4205141e575911
Daw20-09 <omardpana22@gmail.com>
2022-04-28 20:19:49 +0200

    naDatabase3

--- labyrinth/src/Database/Select2.java
+++ labyrinth/src/Database/Select2.java
@@ -14,13 +14,13 @@ public class Select2
     public static void lanzarConsulta()
     {
         try {
-            Class.forName("com.mysql.jc.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.0:3306/agenda", "root", "alpurria22");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.0:3306/labyrinth", "root", "1234");
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM contacto;");
-            while (rs.next()) {
-                System.out.println("Contacto " + rs.getInt("id") + ": " + rs.getString("telefono"));
-            }
+            ResultSet rs = stmt.executeQuery("INSERT INTO users(username,password,name,nif,email,address,birthdate,role) VALUES ('admin','123','administrador','11111111Y','omardpana22@gmail.com','c/Fco de orellana #1 1-H','fechaenSQL','admin';)");
+            /**while (rs.next()) {
+                System.out.println("Users " + rs.getInt("id") + ": " + rs.getString("telefono"));
+            }*/
             rs.close();
             stmt.close();
             conn.close();
